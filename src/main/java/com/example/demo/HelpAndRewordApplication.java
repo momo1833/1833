8f707fd2598eb4d8478c4a98f176ddc1c72a3852
3030931 <3030931@30309-31>
2024-09-20 14:18:46 +0900

    first.commit

---
+++ src/main/java/com/example/demo/HelpAndRewordApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpAndRewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpAndRewordApplication.class, args);
+	}
+
+}
